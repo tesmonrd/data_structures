33abae5049547c5c8b0a4636bdea1e002fbf24dd
Rick Tesmond <tesmonrd@gmail.com>
2016-03-10 15:07:09 -0800

    working on other funcs

--- queue/queue.py
+++ queue/queue.py
@@ -1,42 +1,50 @@
 class Node(object):
-    """L."""
+    """Node class."""
 
     def __init__(self, data, prev=None, next=None):
-        """I."""
+        """Instanitiate the Node class."""
         self.data = data
         self.prev = prev
         self.next = next
 
 
 class Queue(object):
-    """S."""
+    """Quese data structure."""
 
     def __init__(self, head=None, tail=None, p=None):
-        """Q."""
+        """Instanitiate the Queue class."""
         self.head = head
         self.tail = tail
         self.p = p
         self.queue_size = 0
 
     def enqueue(self, data):
-        """V."""
+        """Add the value to the queue."""
         if self.queue_size == 0:
             self.head = Node(data)
             self.tail = Node(data)
             self.queue_size += 1
-            print(self.head.data)
+            # print(self.head.data)
         else:
             new_node = Node(data)
             new_node.prev = self.tail
-            print(new_node.prev.data)
             self.tail = new_node
-            print(self.tail.data)
-            print("****************")
+            self.queue_size += 1
+            # print(new_node.prev.data)
+            # print(self.tail.data)
+            # print("****************")
 
     def size(self):
-        """S."""
+        """Size of Queue."""
+        return self.queue_size
+
+    def dequeue(self, data):
+        """Remove the correct item from the queue."""
         pass
 
+    def peek(self, data):
+        """Return next value in queue w/o dequeueing."""
+        pass
 
 q = Queue()
 q.enqueue(1)
