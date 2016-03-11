1d085d7782c6cf496ca8c1040c58c29790ffab9b
muniri92 <mibrah04@gmail.com>
2016-03-11 13:05:33 -0800

    fix linting issue

--- stack/test_stack.py
+++ stack/test_stack.py
@@ -1,13 +1,13 @@
+"""Testing the Stack data-structure."""
 import pytest
-
 
 PUSH_TEST = [("4", "4")]
 
-COUNT_LIST = [1, 2, 3, 99, "potato"]
+count_list = [1, 2, 3, 99, "potato"]
 
-POP_TEST = [(COUNT_LIST, 4)]
+POP_TEST = [(count_list, 4)]
 
-COUNT_TEST = [(COUNT_LIST, 5)]
+COUNT_TEST = [(count_list, 5)]
 
 
 @pytest.mark.parametrize('value, result', PUSH_TEST)
@@ -18,22 +18,22 @@ def test_push(value, result):
     assert push.push(value) == result
 
 
-@pytest.mark.parametrize('COUNT_LIST, count', COUNT_TEST)
-def test_count(COUNT_LIST, count):
+@pytest.mark.parametrize('count_list, count', COUNT_TEST)
+def test_count(count_list, count):
     """Test to see if stack count is correct."""
     from stack import Stack
     s = Stack()
-    for i in COUNT_LIST:
+    for i in count_list:
         s.push(i)
     assert s.size() == count
 
 
-@pytest.mark.parametrize('COUNT_LIST, result', POP_TEST)
-def test_pop(COUNT_LIST, result):
+@pytest.mark.parametrize('count_list, result', POP_TEST)
+def test_pop(count_list, result):
     """Test to see if last value in stack is popped out."""
     from stack import Stack
     s = Stack()
-    for i in COUNT_LIST:
+    for i in count_list:
         s.push(i)
     s.pop()
     assert s.size() == 4
