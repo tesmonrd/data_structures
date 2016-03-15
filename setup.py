d90dc05db8fa11ad27f14791edf7434d0ae9e823
muniri92 <mibrah04@gmail.com>
2016-03-14 17:31:46 -0700

    add the tox files

---
+++ setup.py
@@ -0,0 +1,16 @@
+# _*_ coding utf-8 _*_
+"""Setup."""
+from setuptools import setup
+
+setup(
+    name="linked_list",
+    description="Linked List Data Structure",
+    version=0.1,
+    author="Munir Ibrahim",
+    author_email="mibrah04@gmail.com",
+    license="MIT",
+    py_modules=["linked"],
+    package_dir={'': 'data_structures'},
+    install_requires=[],
+    extras_require={"test": ["pytest", "pytest-xdist", "tox"]},
+)
