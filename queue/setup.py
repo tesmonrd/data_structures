b3cf26656ca7c28fb3a6041857a0bedfaf94de95
muniri92 <mibrah04@gmail.com>
2016-03-15 21:11:05 -0700

    fixed tox

---
+++ queue/setup.py
@@ -0,0 +1,15 @@
+# _*_ coding utf-8 _*_
+"""Setup."""
+from setuptools import setup
+
+setup(
+    name="queue",
+    description="Queue Data Structure",
+    version=0.1,
+    author="Munir Ibrahim",
+    author_email="mibrah04@gmail.com",
+    license="MIT",
+    py_modules=["queue_2"],
+    install_requires=[],
+    extras_require={"test": ["pytest", "pytest-xdist", "tox"]},
+)
--- queue/test_queue.py
+++ queue/test_queue.py
@@ -23,7 +23,7 @@ PEEK_TEST_ERROR = [(None)]
 @pytest.mark.parametrize('value, result', ENQUEUE_TEST)
 def test_enqueue(value, result):
     """Test to see that values are add to queue."""
-    from queue_2 import Queue
+    from queue import Queue
     q = Queue()
     assert q.enqueue(value) == result
 
@@ -31,7 +31,7 @@ def test_enqueue(value, result):
 @pytest.mark.parametrize("value, result", SIZE_TEST)
 def test_size(value, result):
     """Test see that size of the queue is correct."""
-    from queue_2 import Queue
+    from queue import Queue
     q = Queue()
     for i in value:
         q.enqueue(i)
@@ -41,7 +41,7 @@ def test_size(value, result):
 @pytest.mark.parametrize("value, result", DEQUEUE_TEST)
 def test_dequeue(value, result):
     """Test to see that the correct value is removed from queue."""
-    from queue_2 import Queue
+    from queue import Queue
     q = Queue()
     for i in value:
         q.enqueue(i)
@@ -52,7 +52,7 @@ def test_dequeue(value, result):
 @pytest.mark.parametrize("value, result", PEEK_TEST)
 def test_peek(value, result):
     """Test to see if we can peek at the next value in queue."""
-    from queue_2 import Queue
+    from queue import Queue
     q = Queue()
     for i in value:
         q.enqueue(i)
