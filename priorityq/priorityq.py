d095823171415b1de727b7001e2cdbea43e4eea7
muniri92 <mibrah04@gmail.com>
2016-03-17 09:14:20 -0700

    got more tests, want to implement property

--- priorityq/priorityq.py
+++ priorityq/priorityq.py
@@ -18,6 +18,7 @@ class PriorityQueue(object):
         self.heap.append(item)
         self.size += 1
         self._check_priority(item, self.size - 1)
+        print(self.heap)
         return self.heap[0]
 
     def _check_priority(self, tup, i):
@@ -26,21 +27,23 @@ class PriorityQueue(object):
             if self.heap[(i - 1) // 2][0] < self.heap[i][0]:
                 self.heap[(i - 1) // 2], self.heap[i] = self.heap[i], self.heap[(i - 1) // 2]
             i = (i - 1) // 2
-        return self.heap
+        print("CHECK: " + str(self.heap[0]))
+        return self.heap[0]
 
     def pop(self):
         """Remove the most important item from the queue.."""
         self._raise_to_top()
         self.heap = self._max_child(0)
         self.size -= 1
-        return self.heap
+        return self.heap[0]
 
     def _raise_to_top(self):
         """Switch the bottom and top item to remove the most important one."""
         # Requires O(1) to remove from end of list and O(n) from the beginning
         self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        print("POP: " + str(self.heap[-1]))
         self.heap.pop()
-        return self.heap
+        return self.heap[-1]
 
     def _max_child(self, i):
         """If a child is more important then its parent, swap them."""
@@ -50,7 +53,7 @@ class PriorityQueue(object):
                 i = self._swap(i, self.heap.index(high_child))
         except IndexError:
             pass
-        return self.heap
+        return self.heap[0]
 
     def _swap(self, parent_i, child_i,):
         """Swap the child and parent in list."""
--- priorityq/test_priorityq.py
+++ priorityq/test_priorityq.py
@@ -4,9 +4,19 @@ import pytest
 
 LIST = [(35, "Mulla"), (92, False), (2, "Drizzy"), (93, "Rihanna"), (0, 62)]
 
-END_LIST = [(1000, "Bey"), (93, "Rihanna"), (92, False), (35, "Mulla"), (2, "Drizzy"), (0, 62)]
+LIST_2 = [(93, "Rihanna"), (92, False), (1000, "Bey"), (35, "Mulla"), (2, "Drizzy"), (0, 62)]
+
+# FINAL_LIST = [(1000, "Bey")]
 
 PUSH_TEST = [(LIST, (1000, "Bey"))]
+
+CHECK_TEST = [(LIST_2, 5, (1000, "Bey"))]
+
+POP_TEST = [(LIST_2, (93, "Rihanna"))]
+
+RAISE_TEST = [(LIST_2, (2, "Drizzy"))]
+
+# MAX_ITEM = [(LIST_2,)]
 
 
 @pytest.mark.parametrize("items, result", PUSH_TEST)
@@ -18,3 +28,35 @@ def test_push(items, result):
         pq.insert(idx)
     assert pq.insert((1000, "Bey")) == result
 
+
+@pytest.mark.parametrize("items, size, result", CHECK_TEST)
+def test_check(items, size, result):
+    """A."""
+    from priorityq import PriorityQueue
+    pq = PriorityQueue()
+    for idx in items:
+        pq.insert(idx)
+    assert pq._check_priority((1000, "Bey"), size) == result
+
+
+@pytest.mark.parametrize("items, result", POP_TEST)
+def test_pop(items, result):
+    """A."""
+    from priorityq import PriorityQueue
+    pq = PriorityQueue()
+    for idx in items:
+        pq.insert(idx)
+    assert pq.pop() == result
+
+
+@pytest.mark.parametrize("items, result", RAISE_TEST)
+def test_raise_to_top(items, result):
+    """A."""
+    from priorityq import PriorityQueue
+    pq = PriorityQueue()
+    for idx in items:
+        pq.insert(idx)
+    assert pq._raise_to_top() == result
+
+
+# @pytest.mark.parametrize(", ", )
