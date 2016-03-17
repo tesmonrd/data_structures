96df07d6f86c02af100fc1c2eeb5428aa7034c5f
Rick Tesmond <tesmonrd@gmail.com>
2016-03-16 22:19:24 -0700

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="deque",
     description="Deque data structure.",
     version=0.1,
-    author="Munir Ibrahim",
+    author="Munir Ibrahim and Rick Tesmond",
     author_email="mibrah04@gmail.com",
     license="MIT",
     py_modules=["deque"],
