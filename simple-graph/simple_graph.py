63e129df8e299fce63d889e9e1a84c5d73e9abc7
Rick Tesmond <tesmonrd@gmail.com>
2016-03-17 13:54:13 -0700

    Have addnodes and nodes working

---
+++ simple-graph/simple_graph.py
@@ -0,0 +1,42 @@
+# _*_ coding: utf-8 _*_
+
+
+class Graph(object):
+    """A."""
+
+    def __init__(self):
+        """Initialize graph."""
+        self.graph = {}
+
+    def nodes(self):
+        """List nodes in graph."""
+        node_list = list(self.graph.keys())
+        print(node_list)
+        return node_list
+
+    def edges(self):
+        """List edges in graph."""
+        return self.graph
+
+    def add_node(self, n):
+        """Add new node to the graph."""
+        self.graph[n] = []
+        return self.graph
+
+    def add_edge(self, n1, n2):
+        """Add edge between two nodes."""
+        if n1 or n2 not in self.graph.keys:
+            self.graph.setdefault(n1, n2)
+        else:
+            self.graph[n1].append(n2)
+
+
+
+
+
+g = Graph()
+g.add_node(1)
+g.add_node(4)
+g.nodes()
+g.add_edge(1, 4)
+print(g.graph)
