1f31264de278b2a029c7e6f8920e11fe4319e929
Rick Tesmond <tesmonrd@gmail.com>
2016-03-17 14:25:14 -0700

    working del node

--- simple-graph/simple_graph.py
+++ simple-graph/simple_graph.py
@@ -26,15 +26,28 @@ class Graph(object):
     def add_edge(self, n1, n2):
         """Add edge between two nodes."""
         if n1 and n2 not in self.graph:
-            self.graph.setdefault(n1, n2)
+            self.graph.setdefault(n1, [n2])
             self.add_node(n2)
         else:
-            self.graph[n1] = n2
+            self.graph[n1] = [n2]
+
+    def del_node(self, n):
+        """Delete a node."""
+        if n in self.graph:
+            del self.graph[n]
+        else:
+            raise KeyError("Node not in graph!")
+
+    def del_edge(self, n1, n2):
+        """Delete an edge between two nodes."""
 
 
 g = Graph()
 g.add_node(1)
 g.add_node(4)
+g.add_node(8)
 g.nodes()
+g.add_edge(8, 4)
 g.add_edge(1, 4)
+g.del_node(100)
 print(g.graph)
