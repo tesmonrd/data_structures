9906f1994965cfdf66f0213614aa731ad54ea1d7
Rick Tesmond <tesmonrd@gmail.com>
2016-03-21 15:06:09 -0700

    proper imports

--- graph-traversal/graphtrav.py
+++ graph-traversal/graphtrav.py
@@ -1,7 +1,9 @@
-import graph
-import stack
-import queue
+from simple_graph import Graph
+from stack import Stack
+from queue import Queue
 
 def depth_first(graph, start):
     """A."""
+    # visited = []
+    # stack = start[]
     pass
