c6fd54625b38305d62c826a72c401ed42a6da783
muniri92 <mibrah04@gmail.com>
2016-03-20 22:05:24 -0700

    added more test, and fixed up a tox issue and lint issue

--- double-linked-list/test_ddl.py
+++ double-linked-list/test_ddl.py
@@ -2,11 +2,17 @@
 """Test of DoubleLinkList."""
 import pytest
 
+LIST = [1, 2, 3, 4, 5]
+
+LIST2 = ["python", "1", 100, "hdjdnd", 9090, "401"]
+
 INSERT_TEST = [("hey", "hey"), (5, 5)]
 
 APPEND_TEST = [("YOLO", "YOLO"), (5678, 5678)]
 
-POP_TEST = [(5, 5)]
+POP_TEST = [(LIST, 5), (LIST2, "401")]
+
+SHIFT_TEST = [(LIST, 1), (LIST2, "python")]
 
 
 @pytest.mark.parametrize("data, result", INSERT_TEST)
@@ -23,3 +29,23 @@ def test_append(data, result):
     from double_linked import DoubleLink
     dl = DoubleLink()
     assert dl.append(data) == result
+
+
+@pytest.mark.parametrize("val, result", POP_TEST)
+def test_pop(val, result):
+    """Test to see if the pop function works properly."""
+    from double_linked import DoubleLink
+    dl = DoubleLink()
+    for i in val:
+        dl.insert(i)
+    assert dl.pop() == result
+
+
+@pytest.mark.parametrize("val, result", SHIFT_TEST)
+def test_shift(val, result):
+    """Test to see if the pop function works properly."""
+    from double_linked import DoubleLink
+    dl = DoubleLink()
+    for i in val:
+        dl.insert(i)
+    assert dl.shift() == result
