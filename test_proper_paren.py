9ccf6805ca99124a0ee30b70025f71edd9abd41b
muniri92 <mibrah04@gmail.com>
2016-03-20 22:54:44 -0700

    can't solve the tox problem

--- test_proper_paren.py
+++ test_proper_paren.py
@@ -2,14 +2,14 @@
 """Test Proper Parenthetics."""
 import pytest
 
-PARAMS_TEST = [(u"(The proper amount of () are in (this) comment)", 0),
+PARAMS_TEST = [(u"There ( (are more ( open ( parentheses then closed", 1),
+               (u"(The proper amount of () are in (this) comment)", 0),
                (u"This comment starts with a ))(( closing parentheses", (-1)),
-               (u"(There are more ( open ( parentheses then closed))", 1)
                ]
 
 
-@pytest.mark.parametrize("strings, result", PARAMS_TEST)
-def test_params(strings, result):
-    """Test to make sure that the correct int is return is string is broken."""
+@pytest.mark.parametrize("string, result", PARAMS_TEST)
+def test_params(string, result):
+    """Test to make sure that the correct int is return if string is broken."""
     from proper_parenthetics import parens
-    assert parens(strings) == result
+    assert parens(string) == result
