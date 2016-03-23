f34e4f99576f288c2592de5a7a54569f694fe7ac
Rick Tesmond <tesmonrd@gmail.com>
2016-03-23 15:22:19 -0700

    depth working, everything working

--- graph-traversal/graphtrav.py
+++ graph-traversal/graphtrav.py
@@ -1,12 +1,12 @@
 # __ coding utf-8 __
-
+"""Implement graph traversal."""
 
 from simple_graph import Graph
 from collections import deque
 
 
 def breadth_first(graph, start):
-    """Depth First Traveral using a Deque as a Stack."""
+    """Breadth First Traveral using a Deque as a queue."""
     g = Graph(graph)
     visited, d = [], deque([start])
     while d:
@@ -15,10 +15,28 @@ def breadth_first(graph, start):
             visited.append(vertex)
             if visited not in g.neighbors(vertex):
                 d.extend(g.neighbors(vertex))
-    print(visited)
     return visited
 
 
+def depth_first(graph, start):
+    """Depth first traversal using deque as a stack."""
+    g = Graph(graph)
+    visited, d = [], deque([start])
+    while d:
+        vertex = d.pop()
+        if vertex not in visited:
+            visited.append(vertex)
+            if visited not in g.neighbors(vertex):
+                d.extend(g.neighbors(vertex))
+    return(visited)
+
 if __name__ == "__main__":
-    graph = {'A': ['B', 'C'], 'B': ['D', 'C'], 'C': [], 'D': ['E'], 'E': []}
+    graph = {'A': ['B', 'C', 'Q'],
+             'B': ['C', 'E'],
+             'C': ['D'],
+             'D': ['Q'],
+             'E': [],
+             'Q': ['B']
+             }
     breadth_first(graph, 'A')
+    depth_first(graph, 'A')
